06fcf9dcf77058eee399a4f8d69617b106ccb49d
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-13 13:58:51 -0500

    added config/connection.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const path = require('path');
 const express = require('express');
-//const routes = require('./routes');
+const exhibs = require('express-handlebars');
+
+const hbs = exhibs.create({});
+const routes = require('./controllers/');
 // import sequelize connection
 //const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(routes);
+
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
 
 // sync sequelize models to the database, then turn on the server
 //sequelize.sync({ force: false }).then(() => { 
