73cd609b5c8e89888e7a6d53739cc6662d3c339f
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-13 15:00:55 -0500

    Vote model added

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const path = require('path');
 const express = require('express');
-const exhibs = require('express-handlebars');
-
-const hbs = exhibs.create({});
-const routes = require('./controllers/');
-// import sequelize connection
-//const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//import seqlize connection
+const sequelize = require('./config/connection');
 
-//app.use(routes);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
-
 app.set('view engine', 'handlebars');
+//app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 // sync sequelize models to the database, then turn on the server
-//sequelize.sync({ force: false }).then(() => { 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  }); 
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port ${PORT}!'));
+});
+//console.log(`App listening on port ${PORT}!`)
