2b407525641884d1ba3c9aed68e5d0d70cd0af24
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-15 23:16:41 -0500

    heroku commit

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,15 +1,36 @@
 // //<form id="user-submit">
-// <input class="card" id="post-title">
-// <input class="card" id="post-text">
-// <button id="submit">New Post</button>
+// <input class="card" id="post-title">username
+// <input class="card" id="post-text">email
+// <button id="submit">New Post</button>////password
 
 const submitPost = function (event) {
     event.preventDefault();
+    const postTitle = document.querySelector('#post-title').value.trim();
+    const postText = document.querySelector('#post-text').value.trim();
+    
 
-    usernamegit
+    if (postTitle && postText ) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                postTitle,
+                postText,
+                ////password//
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-
+        const res = await response.json();
+        console.log(res);
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
+
+    
 document.querySelector('#user-submit').addEventListener('submit')
 
 
